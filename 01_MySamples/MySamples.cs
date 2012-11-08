07e1f53a25dd58a423894b05c52719edcb2353b7
devlights <gsf.zero1@gmail.com>
2012-11-08 12:09:25 +0900

    Implement #3
    Add LinqSamples55

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -8217,6 +8217,45 @@ namespace Gsf.Samples
   }
   #endregion
   
+  #region LinqSamples-55
+  /// <summary>
+  /// LINQ to XMLのサンプルです。
+  /// </summary>
+  /// <remarks>
+  /// LINQ to XMLにてエラー発生時XmlExceptionが発生することを確認するサンプルです。
+  /// </remarks>
+  public class LinqSamples55 : IExecutable
+  {
+    public void Execute()
+    {
+      try
+      {
+        //
+        // LINQ to XMLは内部でXmlReaderを利用している.
+        // なので、エラーが発生した場合、XmlReaderの場合と
+        // 同様にXmlExceptionが発生する.
+        //
+        XElement.Parse(GetXmlStrings());
+      }
+      catch(XmlException xmlEx)
+      {
+        Console.WriteLine(xmlEx.ToString());
+      }
+    }
+    
+    string GetXmlStrings()
+    {
+      //
+      // わざと解析エラーになるXML文字列を作成.
+      //
+      return @"<data>
+                 <id>1</id>
+                 <id>2</id>
+               </dat>";
+    }
+  }
+  #endregion
+  
   #region QueueSynchronizedSamples-01
   /// <summary>
   /// Queueの同期処理についてのサンプルです。
