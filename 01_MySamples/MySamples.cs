5ffde472a4769d6102dc672fb2777d638442a960
devlights <gsf.zero1@gmail.com>
2012-11-14 13:54:28 +0900

    Add some code (#6)

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -8413,6 +8413,11 @@ namespace Gsf.Samples
       Console.WriteLine("参照が同じ？ = {0}", noParent == noParent2);
       Console.WriteLine(tree1);
       
+      // 値を変更して確認.
+      noParent.SetValue(false);
+      Console.WriteLine(noParent.Value);
+      Console.WriteLine(tree1.Element("NoParent").Value);
+      
       Console.WriteLine("==========================================");
       
       //
@@ -8423,6 +8428,11 @@ namespace Gsf.Samples
       
       Console.WriteLine("参照が同じ？ = {0}", origTree.Element("WithParent") == tree2.Element("WithParent"));
       Console.WriteLine(tree2);
+      
+      // 値を変更して確認
+      origTree.Element("WithParent").SetValue(false);
+      Console.WriteLine(origTree.Element("WithParent").Value);
+      Console.WriteLine(tree2.Element("WithParent").Value);
     }
   }
   #endregion
