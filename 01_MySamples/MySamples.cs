32f33202bb2bab5b08f2e6147430e3f1ca096106
devlights <gsf.zero1@gmail.com>
2012-11-14 13:11:40 +0900

    Implement #6
    Add LinqSamples58

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -8392,6 +8392,41 @@ namespace Gsf.Samples
   }
   #endregion
   
+  #region LinqSamples-58
+  /// <summary>
+  /// LINQ to XMLのサンプルです.
+  /// </summary>
+  /// <remarks>
+  /// 要素のクローンとアタッチについてのサンプルです.
+  /// </remarks>
+  public class LinqSamples58 : IExecutable
+  {
+    public void Execute()
+    {
+      //
+      // 親要素を持たない要素を作成し、特定のXMLツリーの中に組み込む. (アタッチ)
+      //
+      var noParent = new XElement("NoParent", true);
+      var tree1    = new XElement("Parent", noParent);
+      
+      var noParent2 = tree1.Element("NoParent");
+      Console.WriteLine("参照が同じ？ = {0}", noParent == noParent2);
+      Console.WriteLine(tree1);
+      
+      Console.WriteLine("==========================================");
+      
+      //
+      // 親要素を持つ要素を作成し、特定のXMLツリーの中に組み込む. (クローン)
+      //
+      var origTree = new XElement("Parent", new XElement("WithParent", true));
+      var tree2    = new XElement("Parent", origTree.Element("WithParent"));
+      
+      Console.WriteLine("参照が同じ？ = {0}", origTree.Element("WithParent") == tree2.Element("WithParent"));
+      Console.WriteLine(tree2);
+    }
+  }
+  #endregion
+  
   #region QueueSynchronizedSamples-01
   /// <summary>
   /// Queueの同期処理についてのサンプルです。
