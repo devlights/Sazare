65f335f615bb204593ea8cc0bf7a5491831dfadf
devlights <gsf.zero1@gmail.com>
2012-12-11 13:17:51 +0900

    Add comments

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -8793,11 +8793,20 @@ namespace Gsf.Samples
   #endregion
   
   #region LinqSamples-63
+  /// <summary>
+  /// LINQ to XMLのサンプルです.
+  /// </summary>
+  /// <remarks>
+  /// 要素置換系メソッドのサンプルです.
+  /// </remarks>
   public class LinqSamples63 : IExecutable
   {
     public void Execute()
     {
+      //
       // ReplaceWith(object)
+      //   現在の要素を指定した要素で置き換える.
+      //   
       var root = BuildSampleXml();
       var elem = root.Descendants("Value").First();
       
@@ -8806,7 +8815,16 @@ namespace Gsf.Samples
       Console.WriteLine(root);
       Console.WriteLine("=====================================");
       
+      //
       // ReplaceNodes(object)
+      //   現在の要素の子ノードを指定された要素で置き換える.
+      //   このメソッドはスナップショットセマンティクスという方法で置換処理を行う。
+      //   スナップショットセマンティクスを用いている場合、置換前に事前に置き換える内容のコピーを
+      //   作成してから、置換処理を行うため、現在の要素の状態を元に置換処理を実装することができる。
+      //   (例： LINQ to XMLを利用して、現在の要素内容をクエリし、その結果を置換後として利用する。）
+      //
+      //   尚、このメソッドは属性を削除しない.
+      //
       root = BuildSampleXml();
       elem = root.Elements("Child").First();
       
@@ -8826,7 +8844,16 @@ namespace Gsf.Samples
       Console.WriteLine(root);
       Console.WriteLine("=====================================");
       
+      //
       // ReplaceAll(object)
+      //   現在の要素の子ノードと属性を削除し、指定された要素で置き換える.
+      //   このメソッドは、スナップショットセマンティクスという方法で置換処理を行う。
+      //   スナップショットセマンティクスを用いている場合、置換前に事前に置き換える内容のコピーを
+      //   作成してから、置換処理を行うため、現在の要素の状態を元に置換処理を実装することができる。
+      //   (例： LINQ to XMLを利用して、現在の要素内容をクエリし、その結果を置換後として利用する。)
+      //
+      //   尚、このメソッドは属性を削除するので利用時には注意が必要。
+      //
       root = BuildSampleXml();
       elem = root.Elements("Child").First();
       
