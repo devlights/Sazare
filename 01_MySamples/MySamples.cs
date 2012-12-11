8d5c6b46789ce33843cff6c3c9ba65b4866ef161
devlights <gsf.zero1@gmail.com>
2012-12-12 07:13:32 +0900

    Add implementation

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -8892,6 +8892,76 @@ namespace Gsf.Samples
   }
   #endregion
   
+  #region LinqSamples-64
+  public class LinqSamples64 : IExecutable
+  {
+    public void Execute()
+    {
+      // FirstAttribute
+      var root = BuildSampleXml();
+      var elem = root.Elements("Child").First();
+      
+      var attr = elem.FirstAttribute;
+      
+      Console.WriteLine(attr);
+      Console.WriteLine("{0}=\"{1}\"", attr.Name, attr.Value);
+      Console.WriteLine("=====================================");
+      
+      // LastAttribute
+      root = BuildSampleXml();
+      elem = root.Elements("Child").First();
+      
+      attr = elem.LastAttribute;
+      
+      Console.WriteLine(attr);
+      Console.WriteLine("=====================================");
+      
+      // Attribute(XName)
+      root = BuildSampleXml();
+      elem = root.Elements("Child").First();
+      
+      attr = elem.Attribute("Id2");
+      
+      Console.WriteLine(attr);
+      Console.WriteLine(elem.Attribute("Id3") == null);
+      Console.WriteLine("=====================================");
+      
+      // Attributes()
+      root = BuildSampleXml();
+      elem = root.Elements("Child").First();
+      
+      var attrs = elem.Attributes();
+      
+      Console.WriteLine("Count={0}", attrs.Count());
+      foreach (var a in attrs)
+      {
+        Console.WriteLine("\t{0}", a);
+      }
+      
+      Console.WriteLine("=====================================");
+      
+      // Attributes(XName)
+      root = BuildSampleXml();
+      var elems = root.Descendants();
+      
+      attrs = elems.Attributes("Id");
+      
+      Console.WriteLine("Count={0}", attrs.Count());
+      foreach (var a in attrs)
+      {
+        Console.WriteLine("\t{0}", a);
+      }
+      
+      Console.WriteLine("=====================================");
+    }
+    
+    XElement BuildSampleXml()
+    {
+      return XElement.Parse("<Root><Child Id=\"100\" Id2=\"200\"><Value Id=\"300\">hoge</Value></Child></Root>");
+    }
+  }
+  #endregion
+  
   #region QueueSynchronizedSamples-01
   /// <summary>
   /// Queueの同期処理についてのサンプルです。
