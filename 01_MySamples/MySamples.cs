1c9df8e88e0b9a4a62ba4a67a41efa1377de05f3
devlights <gsf.zero1@gmail.com>
2012-12-12 10:08:34 +0900

    Add LinqSamples65.

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -8984,6 +8984,54 @@ namespace Gsf.Samples
   }
   #endregion
   
+  #region LinqSamples-65
+  public class LinqSamples65 : IExecutable
+  {
+    public void Execute()
+    {
+      // Add(object)
+      var root = BuildSampleXml();
+      var elem = root.Elements("Child").First();
+      
+      elem.Add(new XAttribute("Id3", 400));
+      Console.WriteLine(root);
+      
+      try
+      {
+        elem.Add(new XAttribute("Id2", 500));
+        Console.WriteLine(root);
+      }
+      catch (InvalidOperationException invalidOpEx)
+      {
+        Console.WriteLine("[ERROR] {0}", invalidOpEx.Message);
+      }
+      
+      Console.WriteLine("=====================================");
+      
+      // SetAttributeValue(XName, object)
+      root = BuildSampleXml();
+      elem = root.Elements("Child").First();
+      
+      elem.SetAttributeValue("Id3", 400);
+      Console.WriteLine(elem);
+      
+      elem.SetAttributeValue("Id3", 500);
+      Console.WriteLine(elem);
+      
+      elem.SetAttributeValue("Id3", null);
+      Console.WriteLine(elem);
+      
+      Console.WriteLine(root);
+      Console.WriteLine("=====================================");
+    }
+    
+    XElement BuildSampleXml()
+    {
+      return XElement.Parse("<Root><Child Id=\"100\" Id2=\"200\"><Value Id=\"300\">hoge</Value></Child></Root>");
+    }
+  }
+  #endregion
+  
   #region QueueSynchronizedSamples-01
   /// <summary>
   /// Queueの同期処理についてのサンプルです。
