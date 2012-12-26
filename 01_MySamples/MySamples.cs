e0923c8155898542d61f5e824f04054a841ddbde
devlights <gsf.zero1@gmail.com>
2012-12-26 14:36:33 +0900

    Add LinqSamples70

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -9379,6 +9379,41 @@ namespace Gsf.Samples
     }
   }
   #endregion
+
+  #region LinqSamples-70
+  public class LinqSamples70 : IExecutable
+  {
+    public void Execute()
+    {
+      // HasElements
+      var root       = BuildSampleXml();
+      var child      = root.Elements("Child").First();
+      var grandChild = child.Elements("Value").First();
+
+      Console.WriteLine("root.HasElements: {0}",        root.HasElements);
+      Console.WriteLine("child.HasElements: {0}",       child.HasElements);
+      Console.WriteLine("grand-child.HasElements: {0}", grandChild.HasElements);
+
+      Console.WriteLine("=====================================");
+
+      // HasAttributes
+      root       = BuildSampleXml();
+      child      = root.Elements("Child").First();
+      grandChild = child.Elements("Value").First();
+
+      Console.WriteLine("root.HasAttributes:{0}",        root.HasAttributes);
+      Console.WriteLine("child.HasAttributes:{0}",       child.HasAttributes);
+      Console.WriteLine("grand-child.HasAttributes:{0}", grandChild.HasAttributes);
+
+      Console.WriteLine("=====================================");
+    }
+
+    XElement BuildSampleXml()
+    {
+      return XElement.Parse("<Root><Child Id=\"100\" Id2=\"200\"><Value Id=\"300\">hoge</Value></Child></Root>");
+    }
+  }
+  #endregion
   
   #region QueueSynchronizedSamples-01
   /// <summary>
