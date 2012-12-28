98be5858e3f8e8f8a7988c7c495c2b3e5ee3725e
devlights <gsf.zero1@gmail.com>
2012-12-28 17:00:28 +0900

    Add some comments

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -9486,16 +9486,31 @@ namespace Gsf.Samples
   #endregion
 
   #region LinqSamples-72
+  /// <summary>
+  /// LINQ to XMLのサンプルです.
+  /// </summary>
+  /// <remarks>
+  /// 空要素系プロパティとメソッド (IsEmpty, EmptySequence) のサンプルです.
+  /// </remarks>
   public class LinqSamples72 : IExecutable
   {
     public void Execute()
     {
+      //
       // EmptySequence
+      //   空のIEnumerable<XElement>を返す静的メソッド.
+      //
       var empty = XElement.EmptySequence;
 
       Console.WriteLine("Count={0}", empty.Count());    
 
+      //
       // IsEmpty
+      //   現在の要素が空要素か否かを判定する.
+      //   空要素の条件は、MSDNに記載があり以下の通りとなる.
+      //     「開始タグのみを持ち、終了した空の要素として表される要素だけが、空の要素と見なされます。」
+      //     (http://msdn.microsoft.com/ja-jp/library/system.xml.linq.xelement.isempty.aspx)
+      //
       var root = BuildSampleXmlNoNode();
       Console.WriteLine("IsEmpty={0}", root.IsEmpty);
 
