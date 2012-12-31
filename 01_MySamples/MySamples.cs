acd3dd8279b3ed3a0e2dd1588ffc14b1e104de3e
devlights <gsf.zero1@gmail.com>
2012-12-31 14:01:21 +0900

    Add LinqSamples74

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -9542,7 +9542,7 @@ namespace Gsf.Samples
   /// LINQ to XMLのサンプルです.
   /// </summary>
   /// <remarks>
-  /// PreviousNode, NextNodeプロパティのサンプルです.
+  /// ナビゲーション(PreviousNode, NextNodeプロパティ)のサンプルです.
   /// </remarks>
   public class LinqSamples73 : IExecutable
   {
@@ -9573,6 +9573,40 @@ namespace Gsf.Samples
 
       elem = root.Elements("Child").Last();
       Console.WriteLine("Next node = {0}", elem.NextNode == null);
+    }
+
+    XElement BuildSampleXml()
+    {
+      var root = new XElement("Root",
+        new XElement("Child", "value1"),
+        new XElement("Child", "value2"),
+        new XElement("Child", "value3"),
+        new XElement("Child", "value4")
+      );
+
+      return root;
+    }
+  }
+  #endregion
+
+  #region LinqSamples-74
+  public class LinqSamples74 : IExecutable
+  {
+    public void Execute()
+    {
+      // FirstNode
+      var root = BuildSampleXml();
+      var elem = root.Elements("Child").First();
+
+      Console.WriteLine(root.FirstNode);
+      Console.WriteLine(elem.FirstNode);
+
+      // LastNode
+      root = BuildSampleXml();
+      elem = root.Elements("Child").First();
+
+      Console.WriteLine(root.LastNode);
+      Console.WriteLine(elem.LastNode);
     }
 
     XElement BuildSampleXml()
