f40f1b58362592e6aba39b41089e22497d9fa42f
devlights <gsf.zero1@gmail.com>
2013-01-02 19:40:00 +0900

    Add LinqSamples75

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -9635,6 +9635,38 @@ namespace Gsf.Samples
   }
   #endregion
 
+  #region LinqSamples-75
+  public class LinqSamples75 : IExecutable
+  {
+    public void Execute()
+    {
+      // Parent
+      var root = BuildSampleXml();
+      var elem = root.Elements("Child").First();
+
+      Console.WriteLine("root.Parent = {0}", root.Parent == null ? "null" : root.Parent.ToString());
+      Console.WriteLine("elem.Parent = {0}", elem.Parent);
+
+      var newElem = new XElement("GrandChild", "value5");
+      root.Elements("Child").Last().Add(newElem);
+
+      Console.WriteLine("newElem.Parent = {0}", newElem.Parent);
+    }
+
+    XElement BuildSampleXml()
+    {
+      var root = new XElement("Root",
+        new XElement("Child", "value1"),
+        new XElement("Child", "value2"),
+        new XElement("Child", "value3"),
+        new XElement("Child", "value4")
+      );
+
+      return root;
+    }
+  }
+  #endregion
+
   #region QueueSynchronizedSamples-01
   /// <summary>
   /// Queueの同期処理についてのサンプルです。
