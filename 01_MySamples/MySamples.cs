a72ee12449c98325e686ca0c570eead171b18c12
devlights <gsf.zero1@gmail.com>
2013-01-31 15:08:20 +0900

    Add some comments

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -10222,12 +10222,28 @@ namespace Gsf.Samples
   #endregion
 
   #region LinqSamples-82
+  /// <summary>
+  /// LINQ to XMLのサンプルです.
+  /// </summary>
+  /// <remarks>
+  /// XPath(System.Xml.XPath.Extensions)のサンプルです.
+  /// LINQ to XMLとXPathの比較については
+  ///   http://msdn.microsoft.com/ja-jp/library/vstudio/bb675178.aspx
+  /// に詳細に記載されている.
+  /// </remarks>
   public class LinqSamples82 : IExecutable
   {
     public void Execute()
     {
+      //
       // XPathSelectElements
-      // http://msdn.microsoft.com/ja-jp/library/vstudio/bb675178.aspx
+      //   XPath式を評価して、XElementを取得する
+      //
+      // LINQ to XMLには、XPath用の拡張メソッドが定義されたクラスが存在するため
+      // それを利用する。以下のクラスである.
+      //   System.Xml.XPath.Extensions
+      // 尚、利用するにはSystem.Xml.XPathをusingしておく必要がある.
+      //
       var root = BuildSampleXml();
 
       // XPath指定
@@ -10258,7 +10274,17 @@ namespace Gsf.Samples
 
       Console.WriteLine("=====================================");
 
+      //
       // XPathEvaluate
+      //   XPath式を評価して、結果を取得する
+      //
+      // LINQ to XMLには、XPath用の拡張メソッドが定義されたクラスが存在するため
+      // それを利用する。以下のクラスである.
+      //   System.Xml.XPath.Extensions
+      // 尚、利用するにはSystem.Xml.XPathをusingしておく必要がある.
+      //
+      // XPathEvaluateメソッドは、戻り値がobjectになることに注意。
+      //
       root = BuildSampleXml();
 
       // XPath指定
