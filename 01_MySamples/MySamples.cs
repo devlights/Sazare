be1fd6a147e2f727d83540ef7e7590a84032a580
devlights <gsf.zero1@gmail.com>
2013-01-31 14:56:45 +0900

    Add LinqSamples82

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -50,6 +50,7 @@ namespace Gsf.Samples
   using System.Xaml;
   using System.Xml;
   using System.Xml.Linq;
+  using System.Xml.XPath;
 
   //
   // Alias設定.
@@ -10216,6 +10217,76 @@ namespace Gsf.Samples
       //  see: http://msdn.microsoft.com/ja-jp/library/vstudio/ms256479(v=vs.90).aspx
       //
       return XElement.Load(@"xml/Books.xml");
+    }
+  }
+  #endregion
+
+  #region LinqSamples-82
+  public class LinqSamples82 : IExecutable
+  {
+    public void Execute()
+    {
+      // XPathSelectElements
+      // http://msdn.microsoft.com/ja-jp/library/vstudio/bb675178.aspx
+      var root = BuildSampleXml();
+
+      // XPath指定
+      foreach (var elem in root.XPathSelectElements("Book/Title"))
+      {
+        Console.WriteLine("Value:{0}, Type:{1}", elem, elem.GetType().Name);
+      }
+
+      // XPath指定
+      foreach (var elem in root.XPathSelectElements("//Title"))
+      {
+        Console.WriteLine("Value:{0}, Type:{1}", elem, elem.GetType().Name);
+      }
+
+      Console.WriteLine("=====================================");
+
+      // LINQ to XML
+      foreach (var elem in root.Elements("Book").Elements("Title"))
+      {
+        Console.WriteLine(elem);
+      }
+
+      // LINQ to XML
+      foreach (var elem in root.Descendants("Title"))
+      {
+        Console.WriteLine(elem);
+      }
+
+      Console.WriteLine("=====================================");
+
+      // XPathEvaluate
+      root = BuildSampleXml();
+
+      // XPath指定
+      foreach (var elem in (IEnumerable) root.XPathEvaluate("Book[@id=\"bk102\"]/PublishDate"))
+      {
+        Console.WriteLine("Value:{0}, Type:{1}", elem, elem.GetType().Name);
+      }
+
+      Console.WriteLine("=====================================");
+
+      // LINQ to XML
+      var query = from   book in root.Elements("Book")
+                  where  book.Attribute("id").Value == "bk102"
+                  select book.Element("PublishDate");
+
+      foreach (var elem in query)
+      {
+        Console.WriteLine(elem);
+      }
+    }
+
+    XElement BuildSampleXml()
+    {
+      //
+      // サンプルXMLファイル
+      //  see: http://msdn.microsoft.com/ja-jp/library/vstudio/ms256479(v=vs.90).aspx
+      //
+      return XElement.Load(@"xml/Books.xml");      
     }
   }
   #endregion
