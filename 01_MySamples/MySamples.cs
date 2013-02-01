8c646927546761c2d370f0b4807b2972c80da453
devlights <gsf.zero1@gmail.com>
2013-02-01 15:03:36 +0900

    Add some comments

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -10318,11 +10318,34 @@ namespace Gsf.Samples
   #endregion
 
   #region LinqSamples-83
+  /// <summary>
+  /// LINQ to XMLのサンプルです.
+  /// </summary>
+  /// <remarks>
+  /// XElementとXAttributeの値取得についてのTipです。
+  /// </remarks>
   public class LinqSamples83 : IExecutable
   {
     public void Execute()
     {
+      //
       // XElementとXAttributeの値はキャストしたら取得できる
+      //   http://msdn.microsoft.com/ja-jp/library/vstudio/bb387049.aspx
+      //
+      // 対応しているのは、以下の型の場合.
+      // string
+      // bool,bool?
+      // int,int?
+      // uint,uint?
+      // long,long?
+      // ulong,ulong?
+      // float,float?
+      // double,double?
+      // decimal,decimal?
+      // DateTime,DateTime?
+      // TimeSpan,TimeSpan?
+      // GUID,GUID?
+      //
       var root = BuildSampleXml();
 
       var title  = (string) root.Descendants("Title").FirstOrDefault()    ?? "Nothing";
