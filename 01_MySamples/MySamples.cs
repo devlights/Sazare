9192aa5a3887114cd19ed66a8405ca8186d4028b
devlights <gsf.zero1@gmail.com>
2013-02-04 11:13:32 +0900

    Add some comments

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -10369,16 +10369,26 @@ namespace Gsf.Samples
   #endregion
 
   #region LinqSamples-84
+  /// <summary>
+  /// LINQ to XMLのサンプルです.
+  /// </summary>
+  /// <remarks>
+  /// Changing, Changedイベントについてのサンプルです。
+  /// </remarks>
   public class LinqSamples84 : IExecutable
   {
     public void Execute()
     {
       //
-      // Changing, Changed
-      // XObjectに属するイベント
-      // Changing
+      // Changing, Changedイベントは、どちらもXObjectに属するイベントである.
+      //
+
+      //
+      // Changingイベント
       //   このイベントは、XMLツリーの変更によってのみ発生する。
       //   XMLツリーの作成では発生しないことに注意。
+      // イベント引数として、XObjectChangeEventArgsを受け取る.
+      // XObjectChangeEventArgsは、ObjectChangeというプロパティを持つ.      
       //
       var root = BuildSampleXml();
 
@@ -10387,9 +10397,16 @@ namespace Gsf.Samples
       var book  = root.Elements("Book").First();
       var title = book.Elements("Title").First();
 
+      // 属性値を変更
+      //   Changingイベントなので、イベントハンドラ内にて見えるsenderの値は*更新前*の値となる。 (Change)
       book.Attribute("id").Value = "updated";
+      // 要素の値を変更
+      //   Title要素は内部にXTextを持っているので、まずそれが削除される (Remove)
+      //   その後、更新後の値を持つXTextが設定される. (Add)
       title.Value = "updated";
       title.Remove();
+      // 要素を追加
+      //   要素が追加される (Add)
       book.Add(new XElement("newelem", "hogehoge"));
 
       Console.WriteLine("=====================================");
@@ -10398,7 +10415,7 @@ namespace Gsf.Samples
       // Changed
       //   このイベントは、XMLツリーの変更によってのみ発生する。
       //   XMLツリーの作成では発生しないことに注意。
-      // どちらのイベントもイベント引数として、XObjectChangeEventArgsを受け取る.
+      // イベント引数として、XObjectChangeEventArgsを受け取る.
       // XObjectChangeEventArgsは、ObjectChangeというプロパティを持つ.
       //
       root = BuildSampleXml();
@@ -10408,6 +10425,8 @@ namespace Gsf.Samples
       book  = root.Elements("Book").First();
       title = book.Elements("Title").First();
 
+      // 属性値を変更
+      //   Changedイベントなので、イベントハンドラ内にて見えるsenderの値は*更新後*の値となる。 (Change)
       book.Attribute("id").Value = "updated";
       title.Value = "updated";
       title.Remove();
@@ -10416,11 +10435,13 @@ namespace Gsf.Samples
       Console.WriteLine("=====================================");
     }
 
+    // Changingイベントハンドラ
     void OnNodeChanging(object sender, XObjectChangeEventArgs e)
     {
       Console.WriteLine("Changing: sender--{0}:{1}, ObjectChange--{2}", sender.GetType().Name, sender, e.ObjectChange);
     }
 
+    // Changedイベントハンドラ
     void OnNodeChanged(object sender, XObjectChangeEventArgs e)
     {
       Console.WriteLine("Changed: sender--{0}:{1}, ObjectChange--{2}", sender.GetType().Name, sender, e.ObjectChange); 
