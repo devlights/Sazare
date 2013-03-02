1fc62cd2dd6b0b478fd533a683ad7ed49c6e300d
devlights <gsf.zero1@gmail.com>
2013-03-02 13:03:07 +0900

    Separate datatable extension method sample

---
+++ 01_MySamples/AdoNet/DataTableExtensionsSample01.cs
@@ -0,0 +1,59 @@
+namespace Gsf.Samples
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Data;
+  using System.Linq;
+
+  #region DataTableExtensionsSample-01
+  /// <summary>
+  /// System.Data.ExtensionsÇÃÉTÉìÉvÉã1Ç≈Ç∑ÅB
+  /// </summary>
+  public class DataTableExtensionsSample01 : IExecutable
+  {
+    public void Execute()
+    {
+      DataTable table = BuildSampleTable();
+      PrintTable("Before:", table);
+
+      IEnumerable<DataRow> query = from row in table.AsEnumerable()
+                                   where (row.Field<int>("COL-1") % 2) != 0
+                                   select row;
+
+      DataTable newTable = query.CopyToDataTable();
+      PrintTable("After:", newTable);
+    }
+
+    DataTable BuildSampleTable()
+    {
+      DataTable table = new DataTable();
+
+      table.BeginInit();
+      table.Columns.Add("COL-1", typeof(int));
+      table.Columns.Add("COL-2");
+      table.EndInit();
+
+      table.BeginLoadData();
+      for (int i = 0; i < 5; i++)
+      {
+        table.LoadDataRow(new object[] { i, (i + 1).ToString() }, true);
+      }
+      table.EndLoadData();
+
+      return table;
+    }
+
+    void PrintTable(string title, DataTable table)
+    {
+      Console.WriteLine(title);
+
+      foreach (DataRow row in table.Rows)
+      {
+        Console.WriteLine("\t{0}, {1}", row[0], row[1]);
+      }
+
+      Console.WriteLine();
+    }
+  }
+  #endregion
+}
---
+++ 01_MySamples/AdoNet/DataTableExtensionsSample02.cs
@@ -0,0 +1,67 @@
+namespace Gsf.Samples
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Data;
+  using System.Linq;
+
+  #region DataTableExtensionsSample-02
+  /// <summary>
+  /// System.Data.Extensionsのサンプル2です。
+  /// </summary>
+  public class DataTableExtensionsSample02 : IExecutable
+  {
+
+    public void Execute()
+    {
+      DataTable table = BuildSampleTable();
+
+      //
+      // 1列目の情報をint型で取得.
+      // 2列目の情報をstring型で取得.
+      //
+      int val1 = table.Rows[0].Field<int>("COL-1");
+      string val2 = table.Rows[0].Field<string>("COL-2");
+      PrintTable("Before:", table);
+
+      //
+      // 1列目の情報を変更.
+      //
+      table.Rows[0].SetField<int>("COL-1", 100);
+      PrintTable("After:", table);
+
+    }
+
+    DataTable BuildSampleTable()
+    {
+      DataTable table = new DataTable();
+
+      table.BeginInit();
+      table.Columns.Add("COL-1", typeof(int));
+      table.Columns.Add("COL-2");
+      table.EndInit();
+
+      table.BeginLoadData();
+      for (int i = 0; i < 5; i++)
+      {
+        table.LoadDataRow(new object[] { i, (i + 1).ToString() }, true);
+      }
+      table.EndLoadData();
+
+      return table;
+    }
+
+    void PrintTable(string title, DataTable table)
+    {
+      Console.WriteLine(title);
+
+      foreach (DataRow row in table.Rows)
+      {
+        Console.WriteLine("\t{0}, {1}", row[0], row[1]);
+      }
+
+      Console.WriteLine();
+    }
+  }
+  #endregion
+}
