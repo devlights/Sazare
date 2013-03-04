b56789afbfe6fe7da8817f39d2f426b0a1f919d8
devlights <gsf.zero1@gmail.com>
2013-03-04 16:40:26 +0900

    Separate LINQ samples

---
+++ 01_MySamples/Linq/LinqSamples03.cs
@@ -0,0 +1,152 @@
+namespace Gsf.Samples
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  #region LinqSamples-03
+  /// <summary>
+  /// LinqÇÃÉTÉìÉvÉãÇ≈Ç∑ÅB
+  /// </summary>
+  public class LinqSamples03 : IExecutable
+  {
+    class Person
+    {
+      public string Id
+      {
+        get;
+        set;
+      }
+      public string Name
+      {
+        get;
+        set;
+      }
+      public AddressInfo Address
+      {
+        get;
+        set;
+      }
+    }
+
+    class AddressInfo
+    {
+      public string PostCode
+      {
+        get;
+        set;
+      }
+      public string Prefecture
+      {
+        get;
+        set;
+      }
+      public string Municipality
+      {
+        get;
+        set;
+      }
+      public string HouseNumber
+      {
+        get;
+        set;
+      }
+      public string[] Tel
+      {
+        get;
+        set;
+      }
+      public string[] Frends
+      {
+        get;
+        set;
+      }
+    }
+
+    IEnumerable<Person> CreateSampleData()
+    {
+      return new Person[]{
+           new Person{ 
+               Id="00001"
+              ,Name="gsf_zero1"
+              ,Address=new AddressInfo{
+                       PostCode="999-8888"
+                      ,Prefecture="ìåãûìs"
+                      ,Municipality="Ç«Ç±Ç©ÇP"
+                      ,HouseNumber="î‘ínÇP"
+                      ,Tel=new []{"090-xxxx-xxxx"}
+                      ,Frends=new string[]{}
+              }
+           }
+          ,new Person{ 
+               Id="00002"
+              ,Name="gsf_zero2"
+              ,Address=new AddressInfo{
+                       PostCode="888-7777"
+                      ,Prefecture="ãûìsï{"
+                      ,Municipality="Ç«Ç±Ç©ÇQ"
+                      ,HouseNumber="î‘ínÇQ"
+                      ,Tel=new []{"080-xxxx-xxxx"}
+                      ,Frends=new []{"00001"}
+              }
+          }
+          ,new Person{ 
+               Id="00003"
+              ,Name="gsf_zero3"
+              ,Address=new AddressInfo{
+                       PostCode="777-6666"
+                      ,Prefecture="ñkäCìπ"
+                      ,Municipality="Ç«Ç±Ç©ÇR"
+                      ,HouseNumber="î‘ínÇR"
+                      ,Tel=new []{"070-xxxx-xxxx"}
+                      ,Frends=new []{"00001", "00002"}
+              }
+          }
+          ,new Person{ 
+               Id="00004"
+              ,Name="gsf_zero4"
+              ,Address=new AddressInfo{
+                       PostCode="777-6666"
+                      ,Prefecture="ñkäCìπ"
+                      ,Municipality="Ç«Ç±Ç©ÇS"
+                      ,HouseNumber="î‘ínÇS"
+                      ,Tel=new []{"060-xxxx-xxxx", "111-111-1111", "222-222-2222"}
+                      ,Frends=new []{"00001", "00003"}
+              }
+          }
+        };
+    }
+
+    public void Execute()
+    {
+      IEnumerable<Person> persons = CreateSampleData();
+
+      //
+      // íäèoÇµÇΩÉIÉuÉWÉFÉNÉgÇÇªÇÃÇ‹Ç‹ï‘Ç∑
+      //
+      var query1 = from person in persons
+                   select person;
+
+      foreach (var person in query1)
+      {
+        Console.WriteLine("Id={0}, Name={1}", person.Id, person.Name);
+      }
+
+      //
+      // íäèoåãâ Ç©ÇÁêVÇΩÇ»å^ÇçÏê¨Ç∑ÇÈ
+      // 
+      var query2 = from person in persons
+                   select new
+                   {
+                     Id = person.Id,
+                     Name = person.Name
+                   };
+
+      foreach (var person in query2)
+      {
+        Console.WriteLine("Id={0}, Name={1}", person.Id, person.Name);
+      }
+    }
+  }
+  #endregion
+}
