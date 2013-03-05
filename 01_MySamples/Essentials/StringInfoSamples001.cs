391159287e5b4709ed1d44e55751c383695b4981
devlights <gsf.zero1@gmail.com>
2013-03-05 16:37:02 +0900

    Separate some samples

---
+++ 01_MySamples/Essentials/StringInfoSamples001.cs
@@ -0,0 +1,100 @@
+namespace Gsf.Samples
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Drawing;
+  using System.Globalization;
+  using System.Linq;
+
+  //
+  // Alias設定.
+  //
+  using GDISize = System.Drawing.Size;
+  using WinFormsApplication = System.Windows.Forms.Application;
+  using WinFormsButton = System.Windows.Forms.Button;
+  using WinFormsControl = System.Windows.Forms.Control;
+  using WinFormsDockStyle = System.Windows.Forms.DockStyle;
+  using WinFormsFlowDirection = System.Windows.Forms.FlowDirection;
+  using WinFormsFlowLayoutPanel = System.Windows.Forms.FlowLayoutPanel;
+  using WinFormsForm = System.Windows.Forms.Form;
+  using WinFormsFormStartPosition = System.Windows.Forms.FormStartPosition;
+  using WinFormsMessageBox = System.Windows.Forms.MessageBox;
+  using WinFormsTextBox = System.Windows.Forms.TextBox;
+
+  #region StringInfoSamples-001
+  /// <summary>
+  /// StringInfoについてのサンプルです。
+  /// </summary>
+  /// <remarks>
+  /// サロゲートペアについて記述しています。
+  /// </remarks>
+  public class StringInfoSamples001 : IExecutable
+  {
+    public class StringInfoSampleForm : WinFormsForm
+    {
+      public StringInfoSampleForm()
+      {
+        InitializeComponent();
+      }
+
+      void InitializeComponent()
+      {
+        SuspendLayout();
+
+        Size = new GDISize(350, 100);
+        StartPosition = WinFormsFormStartPosition.CenterScreen;
+        Text = "サロゲートペアの確認サンプル";
+
+
+        WinFormsTextBox t = new WinFormsTextBox();
+        t.Text = "\uD867\uDE3D"; // 魚へんに花という文字。魚のホッケの文字を指定。
+
+        WinFormsButton b = new WinFormsButton { Text = "Exec" };
+        b.Click += (s, e) =>
+        {
+
+          string str = t.Text;
+          WinFormsMessageBox.Show(string.Format("文字：{0}, 長さ：{1}", str, str.Length), "Stringでの表示");
+
+          //
+          // サロゲートペアの文字列
+          //
+          // サロゲートペアの文字列は１文字で
+          // ４バイトとなっている。
+          //
+          // サロゲートペアの文字列に対して
+          // String.Lengthプロパティで長さを取得すると
+          // １文字なのに２と返ってくる。
+          //
+          // これを１文字として認識するには以下のクラスを利用する。
+          //
+          // System.Globalization.StringInfo
+          //
+          // このクラスの以下のプロパティを利用することで
+          // １文字と認識することが出来る。
+          //
+          // LengthInTextElementsプロパティ
+          //
+          StringInfo si = new StringInfo(str);
+          WinFormsMessageBox.Show(string.Format("文字：{0}, 長さ：{1}", si.String, si.LengthInTextElements), "StringInfoでの表示");
+        };
+
+        WinFormsFlowLayoutPanel contentPane = new WinFormsFlowLayoutPanel { FlowDirection = WinFormsFlowDirection.TopDown, WrapContents = true };
+        contentPane.Controls.AddRange(new WinFormsControl[] { t, b });
+        contentPane.Dock = WinFormsDockStyle.Fill;
+
+        Controls.Add(contentPane);
+
+        ResumeLayout();
+      }
+    }
+
+    [STAThread]
+    public void Execute()
+    {
+      WinFormsApplication.EnableVisualStyles();
+      WinFormsApplication.Run(new StringInfoSampleForm());
+    }
+  }
+  #endregion
+}
---
+++ 01_MySamples/Essentials/TimeConvertSample02.cs
@@ -0,0 +1,53 @@
+namespace Gsf.Samples
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  #region 時刻に関する処理(10進数形式からXX時間XX分形式に変換)
+  /// <summary>
+  /// 時刻に関する処理(10進数形式からXX時間XX分形式に変換)についてのサンプルです。
+  /// </summary>
+  public class TimeConvertSample02 : IExecutable
+  {
+    public void Execute()
+    {
+      // 元の値. 7.67時間とする.
+      decimal original = 111.12M;
+
+      //
+      // 時間の部分は既に確定済みなので、そのまま利用.
+      //
+      int hour = decimal.ToInt32(original);
+
+      //
+      // 時間部分の分数を算出.
+      //
+      int hourMinutes = (hour * 60);
+
+      //
+      // 元の値の分数を算出.
+      //
+      decimal originalMinutes = (original * 60);
+
+      //
+      // 求めた元の値の分数を四捨五入.
+      //
+      int roundedOriginalMinutes = decimal.ToInt32(Math.Round(originalMinutes, 0, MidpointRounding.AwayFromZero));
+
+      //
+      // 元の値の分数から時間部分の分数を引く.
+      // これが結果の分数となる。
+      //
+      int minutes = (roundedOriginalMinutes - hourMinutes);
+
+      //
+      // 結果を構築.
+      //
+      decimal result = decimal.Parse(string.Format("{0}.{1}", hour, minutes));
+
+      Console.WriteLine("結果={0}, {1}時間{2}分", result, hour, minutes);
+    }
+  }
+  #endregion
+}
