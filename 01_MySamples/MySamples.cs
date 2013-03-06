a7d0d428909f0a04e0d84708995f8487d94f7bab
devlights <gsf.zero1@gmail.com>
2013-03-06 15:07:29 +0900

    Separate a ADO.NET sample

--- 01_MySamples/MySamples.cs
+++ 01_MySamples/MySamples.cs
@@ -117,52 +117,6 @@ namespace Gsf.Samples
   }
   #endregion
   
-  #region DbCommandTimeoutSample-01
-  /// <summary>
-  /// DbCommandのタイムアウト機能についてのサンプルです。
-  /// </summary>
-  public class DbCommandTimeoutSample01 : IExecutable
-  {
-    public void Execute()
-    {
-      var factory = DbProviderFactories.GetFactory("System.Data.SqlClient");
-      using (var conn = factory.CreateConnection())
-      {
-        conn.ConnectionString = @"User Id=medal;Password=medal;Initial Catalog=Medal;Data Source=.\SQLEXPRESS";
-        conn.Open();
-        
-        using (var command = conn.CreateCommand())
-        {
-          command.CommandText = @"SELECT a.*, b.* FROM MST_ZIP a FULL OUTER JOIN MST_ZIP b ON a.[publicCode] = b.[publicCode] WHERE a.[cmp] LIKE '%あ%' AND b.[cmpK] LIKE '%ｱ%'";
-          command.CommandTimeout = 1;
-          
-          try
-          {
-            var watch = Stopwatch.StartNew();
-            using (var reader = command.ExecuteReader())
-            {
-              watch.Stop();
-              
-              var count = 0;
-              /*
-              for (; reader.Read(); count++)
-              {
-              }
-              */
-              
-              Console.WriteLine("COUNT={0}, Elapsed={1}", count, watch.Elapsed);
-            }
-          }
-          catch(DbException ex)
-          {
-            Console.WriteLine(ex);
-          }
-        }
-      }
-    }
-  }
-  #endregion
-  
   #region ByteArraySamples-01
   /// <summary>
   /// バイト配列についてのサンプルです。
