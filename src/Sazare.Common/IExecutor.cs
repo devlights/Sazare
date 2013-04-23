41c58be1c46569c3e4c7e05818617cc64e42ff24
devlights <gsf.zero1@gmail.com>
2013-04-23 16:50:58 +0900

    define interfaces and classes

---
+++ src/Sazare.Common/IExecutor.cs
@@ -0,0 +1,10 @@
+﻿namespace Sazare.Common
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public interface IExecutor
+  {
+  }
+}
---
+++ src/Sazare.Tools.Cui/ConsoleKeyManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Sazare.Tools.Cui
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class ConsoleKeyManager
+  {
+  }
+}
