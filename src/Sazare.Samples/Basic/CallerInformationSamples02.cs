23fe90b454a59f4f94ceb3feba18107673f6436c
devlights <gsf.zero1@gmail.com>
2013-06-24 22:11:17 +0900

    add CallerInformationSamples02

---
+++ src/Sazare.Samples/Basic/CallerInformationSamples02.cs
@@ -0,0 +1,54 @@
+﻿namespace Sazare.Samples
+{
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel;
+  using System.Linq;
+  using System.Runtime.CompilerServices;
+
+  using Sazare.Common;
+  
+  public class CallerInformationSamples02 : IExecutable
+  {
+    public void Execute()
+    {
+      //
+      // 普通にINotifyPropertyChangedの実装クラスを使って値を変更して確認.
+      //
+      var notifyObj = new NotityPropertyChangedImpl();
+
+      notifyObj.PropertyChanged += (s, e) => { Console.WriteLine("[{0}] changed to [{1}]", e.PropertyName, (s as dynamic).Name); };
+
+      notifyObj.Name = "hello world";
+      notifyObj.Name = "goobye world";
+    }
+
+    class NotityPropertyChangedImpl : INotifyPropertyChanged
+    {
+      private string _name;
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      public string Name
+      {
+        get { return _name; }
+        set
+        {
+          if (_name == value)
+            return;
+          _name = value;
+          OnPropertyChanged();
+        }
+      }
+      
+      #region OnPropertyChanged
+      public virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+      {
+        PropertyChangedEventHandler handler = PropertyChanged;
+        if (handler != null)
+          handler(this, new PropertyChangedEventArgs(propertyName));
+      }
+      #endregion
+    }
+  }
+}
