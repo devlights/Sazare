79563f918bdad1488074c55395cdf3bf54103c47
devlights <gsf.zero1@gmail.com>
2013-09-03 10:35:52 +0900

    add class

---
+++ src/Sazare.Samples/Basic/BufferSamples01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sazare.Common;
+
+namespace Sazare.Samples.Basic
+{
+  public class BufferSamples01 : IExecutable
+  {
+    public void Execute()
+    {
+      //
+      // System.Bufferクラスのサンプル
+      //   http://msdn.microsoft.com/ja-jp/library/system.buffer.aspx
+      //
+
+    }
+  }
+}
