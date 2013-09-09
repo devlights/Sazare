d294d816610cdb22234533853d98af5115062c5f
devlights <gsf.zero1@gmail.com>
2013-09-09 20:27:33 +0900

    Add TestCase2...modify

---
+++ src/Tests/Sazare.Common.Tests/InputTest.cs
@@ -0,0 +1,80 @@
+﻿namespace Sazare.Common
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.IO;
+
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [TestClass]
+  public class InputTest
+  {
+    [TestCleanup]
+    public void Cleanup()
+    {
+      Input.SetInputManager(null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void InputManagerを設定していない状態で操作すると例外が発生する()
+    {
+      // Arrange
+      // Act
+      Input.Read();
+
+      // Assert
+      Assert.Fail("例外が発生していない");
+    }
+
+    [TestMethod]
+    public void InputManagerを設定することができる()
+    {
+      // Arrange
+      // Act
+      Input.SetInputManager(new MockInputManager());
+
+      // Assert
+    }
+
+    [TestMethod]
+    public void Readメソッドでデータを取得することができる()
+    {
+      // Arrange
+      Input.SetInputManager(new MockInputManager());
+
+      // Act
+      object data = Input.Read();
+
+      // Assert
+      Assert.AreEqual("t", data);
+    }
+
+    [TestMethod]
+    public void ReadLineメソッドでデータを取得することができる()
+    {
+      // Arrange
+      Input.SetInputManager(new MockInputManager());
+
+      // Assert
+      object data = Input.ReadLine();
+
+      // Act
+      Assert.AreEqual(string.Format("test data\n"), data);
+    }
+
+    class MockInputManager : IInputManager
+    {
+      public object Read()
+      {
+        return "t";
+      }
+
+      public object ReadLine()
+      {
+        return string.Format("{0}\n", "test data", Read());
+      }
+    }
+  }
+}
