381ed466b45648b34a388f40c4a4b7e19b0287e8
devlights <gsf.zero1@gmail.com>
2013-11-12 11:46:12 +0900

    Added class

---
+++ src/Sazare.Samples/Basic/DebugSamples01.cs
@@ -0,0 +1,23 @@
+﻿namespace Sazare.Samples
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Diagnostics;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  using Sazare.Common;
+
+  /// <summary>
+  /// Debugクラスについてのサンプルです。
+  /// </summary>
+  [Sample]
+  public class DebugSamples01 : IExecutable
+  {
+    public void Execute()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
