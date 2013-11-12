19e4bd24e166024c91912e9cb09c6187562fdeab
devlights <gsf.zero1@gmail.com>
2013-11-13 01:15:50 +0900

    Added some comments

--- src/Sazare.Samples/Basic/DebugSamples01.cs
+++ src/Sazare.Samples/Basic/DebugSamples01.cs
@@ -17,7 +17,26 @@
   {
     public void Execute()
     {
-      throw new NotImplementedException();
+      //
+      // Debugクラスは文字通りデバッグ情報を出力するために利用されるクラスである.
+      // このクラスの全てのメソッドは、Conditional("DEBUG")属性が付与されている.
+      // なので、DEBUG定数を指定していない場合、コンパイラの段階で呼び出しが削除される.
+      //
+      // Visual Studioではデフォルトで
+      //   デバッグビルドの場合は、DEBUGとTRACE
+      //   リリースビルドの場合は、TRACE
+      // が付与されて、コンパイルされる.
+      //
+      // Traceクラスと同様にDebugクラスもListenerの考えを持っていて
+      // リスナーを追加することでいろいろな出力が行える様になっている.
+      //
+      // 注意点として、ListenerはTraceとDebugで共有されており
+      // どちらかに追加すると両方で有効となる.
+      //
+      bool output = true;
+
+      Debug.WriteLine("デバッグメッセージ");
+      Debug.WriteLineIf(output, "条件付きデバッグメッセージ");
     }
   }
 }
