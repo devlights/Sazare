8ce0b3e952f771c68b06d032911aae95adbe6c18
devlights <gsf.zero1@gmail.com>
2014-01-13 22:31:53 +0900

    add IAsyncExecutable interface

---
+++ src/Sazare.Common/IAsyncExecutable.cs
@@ -0,0 +1,19 @@
+﻿namespace Sazare.Common
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Threading.Tasks;
+
+  /// <summary>
+  /// 非同期実行できることを示すインターフェースです。
+  /// </summary>
+  public interface IAsyncExecutable : IExecutable
+  {
+    /// <summary>
+    /// 処理を実行します。
+    /// </summary>
+    /// <returns>タスク</returns>
+    Task Execute();
+  }
+}
