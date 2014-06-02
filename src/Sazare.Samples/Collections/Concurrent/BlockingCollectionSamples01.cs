67ff7c72989f862ba4ab17e251f245c3f8f62258
devlights <gsf.zero1@devlights.onmicrosoft.com>
2014-06-02 18:29:44 +0900

    クラス追加

---
+++ src/Sazare.Samples/Collections/Concurrent/BlockingCollectionSamples01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sazare.Common;
+
+namespace Sazare.Samples.Collections.Concurrent
+{
+  /// <summary>
+  /// System.Collections.Concurrent.BlockingCollectionクラスのサンプルです。
+  /// </summary>
+  [Sample]
+  public class BlockingCollectionSamples01 : IExecutor
+  {
+    public void Execute(IExecutable target)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
