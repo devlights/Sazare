7dde2ac7c9153741ca5bcd531b1b797fb9b8175c
devlights <gsf.zero1@devlights.onmicrosoft.com>
2015-09-11 17:50:34 +0900

    コメント追加

--- src/Sazare.Samples/Basic/BitConverterSamples02.cs
+++ src/Sazare.Samples/Basic/BitConverterSamples02.cs
@@ -9,19 +9,37 @@ namespace Sazare.Samples
   using Sazare.Samples.BitConverterSamples02_Extensions;
 
   #region BitConverterSamples-02
-
+  /// <summary>
+  /// System.BitConverterクラスのサンプルです。
+  /// </summary>
+  /// <remarks>
+  /// BitConverterがデフォルトでは対応していない
+  /// decimalとDateTimeのバイナリ化についてのサンプルです。
+  /// </remarks>
   public class BitConverterSamples02 : IExecutable
   {
     public void Execute()
     {
       var hexConverter = new Func<byte, string>(i => i.ToString("x"));
 
+      //
+      // decimal型のバイト列変換
+      //   decimal.GetBitsメソッドを使用することで
+      //   対象のdecimal値の内部表現をintの配列で取得できる。
+      //   後は、それをBitConverterを使ってバイト列にする。
+      //
       decimal.GetBits(decimal.MaxValue)
         .SelectMany(BitConverter.GetBytes)
         .JoinAndPrint(Output.WriteLine, hexConverter);
 
       Output.WriteLine();
 
+      //
+      // DateTimeのバイト列変換
+      //   DateTime.ToBinaryメソッドを使用することで
+      //   対象のDateTime値をlongで取得できる。
+      //   後は、それをBitConverterを使ってバイト列にする。
+      //
       BitConverter.GetBytes(DateTime.MaxValue.ToBinary())
         .JoinAndPrint(Output.WriteLine, hexConverter);
     }
@@ -34,8 +52,8 @@ namespace Sazare.Samples
       public static void JoinAndPrint<T>(this IEnumerable<T> self, Action<object> action, Func<T, string> hexConverter, string prefix = "0x")
       {
         var enumerable = self as T[] ?? self.ToArray();
-
         var queryHex = enumerable.Select(hexConverter);
+
         action(string.Join(" ", enumerable));
         action(string.Join(" ", queryHex));
       }
