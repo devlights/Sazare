71805460c7bf594aa591cc1eae2c3db84bf1f4b1
devlights <gsf.zero1@devlights.onmicrosoft.com>
2016-08-30 14:26:43 +0900

    Helpersの下をクリーンアップ完了。

--- src/Sazare.Samples/Helpers/CompareResultHelper.cs
+++ src/Sazare.Samples/Helpers/CompareResultHelper.cs
@@ -1,24 +1,22 @@
+// ReSharper disable CheckNamespace
 namespace Sazare.Samples
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
 
-  using Sazare.Common;
-  
-  #region CompareOptionsSamples-01
-  /// <summary>
-  /// 比較メソッドの結果値を変換するためのヘルパークラス.
-  /// </summary>
-  public static class CompareResultHelper
-  {
-    static readonly string[] CompResults = { "小さい", "等しい", "大きい" };
+    #region CompareOptionsSamples-01
 
-    // 比較結果の数値を文字列に変換.
-    public static string ToStringResult(this int self)
+    /// <summary>
+    ///     比較メソッドの結果値を変換するためのヘルパークラス.
+    /// </summary>
+    public static class CompareResultHelper
     {
-      return CompResults[self + 1];
+        private static readonly string[] CompResults = {"小さい", "等しい", "大きい"};
+
+        // 比較結果の数値を文字列に変換.
+        public static string ToStringResult(this int self)
+        {
+            return CompResults[self + 1];
+        }
     }
-  }
-  #endregion
+
+    #endregion
 }
