38519680d9d22d5750c3ddb8d411dd5399621c25
devlights <gsf.zero1@devlights.onmicrosoft.com>
2016-08-31 11:34:11 +0900

    Networkingの下をクリーンアップ完了。

--- src/Sazare.Samples/NetWorking/PingSamples01.cs
+++ src/Sazare.Samples/NetWorking/PingSamples01.cs
@@ -1,73 +1,73 @@
+// ReSharper disable CheckNamespace
+
+using System.Net.NetworkInformation;
+using System.Threading;
+using Sazare.Common;
+
 namespace Sazare.Samples
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Net.NetworkInformation;
-  using System.Threading;
 
-  using Sazare.Common;
-  
-  #region PingSamples-01
-  /// <summary>
-  /// Pingクラスに関するサンプルです。
-  /// </summary>
-  [Sample]
-  public class PingSamples01 : Sazare.Common.IExecutable
-  {
-    public void Execute()
+    #region PingSamples-01
+
+    /// <summary>
+    ///     Pingクラスに関するサンプルです。
+    /// </summary>
+    [Sample]
+    public class PingSamples01 : IExecutable
     {
-      //
-      // 同期での送信.
-      // 
-      string hostName = "localhost";
-      int timeOut = 3000;
-
-      Ping p = new Ping();
-      PingReply r = p.Send(hostName, timeOut);
-
-      if (r.Status == IPStatus.Success)
-      {
-        Output.WriteLine("Ping.Send() Success.");
-      }
-      else
-      {
-        Output.WriteLine("Ping.Send() Failed.");
-      }
-
-      //
-      // 非同期での送信.
-      //
-      hostName = "www.google.com";
-      object userToken = null;
-
-      p.PingCompleted += (s, e) =>
-      {
-
-        if (e.Cancelled)
+        public void Execute()
         {
-          Output.WriteLine("Cancelled..");
-          return;
+            //
+            // 同期での送信.
+            // 
+            var hostName = "localhost";
+            var timeOut = 3000;
+
+            var p = new Ping();
+            var r = p.Send(hostName, timeOut);
+
+            if (r.Status == IPStatus.Success)
+            {
+                Output.WriteLine("Ping.Send() Success.");
+            }
+            else
+            {
+                Output.WriteLine("Ping.Send() Failed.");
+            }
+
+            //
+            // 非同期での送信.
+            //
+            hostName = "www.google.com";
+            object userToken = null;
+
+            p.PingCompleted += (s, e) =>
+            {
+                if (e.Cancelled)
+                {
+                    Output.WriteLine("Cancelled..");
+                    return;
+                }
+
+                if (e.Error != null)
+                {
+                    Output.WriteLine(e.Error.ToString());
+                    return;
+                }
+
+                if (e.Reply.Status != IPStatus.Success)
+                {
+                    Output.WriteLine("Ping.SendAsync() Failed");
+                    return;
+                }
+
+                Output.WriteLine("Ping.SendAsync() Success.");
+            };
+
+            p.SendAsync(hostName, timeOut, userToken);
+            Thread.Sleep(3000);
         }
-
-        if (e.Error != null)
-        {
-          Output.WriteLine(e.Error.ToString());
-          return;
-        }
-
-        if (e.Reply.Status != IPStatus.Success)
-        {
-          Output.WriteLine("Ping.SendAsync() Failed");
-          return;
-        }
-
-        Output.WriteLine("Ping.SendAsync() Success.");
-      };
-
-      p.SendAsync(hostName, timeOut, userToken);
-      Thread.Sleep(3000);
     }
-  }
-  #endregion
+
+    #endregion
 }
